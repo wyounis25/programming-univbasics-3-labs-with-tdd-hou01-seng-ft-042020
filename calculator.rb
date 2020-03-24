7751e63a53c5f869c82f13fa75021c2bcb3b7060
walid younis <walid.younis.90@gmail.com>
2020-03-24 16:41:56 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
-# Add your variables here
+# Add your variables 
+first_number = 3.0 
+second_number = 5
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
